2c29e7d1ed9d321362b3c7bc427716d720fb250c
Pavel Soriano <p.soriano@has-sante.fr>
2021-10-04 16:47:19 +0200

    fixed docker port

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@ echo "Build image"
 docker build -t datascience-experiment .
 
 echo "Run container"
-docker run -d -p 8050:80 --name datascience-experiment datascience-experiment
+docker run -d -p 8502:80 --name datascience-experiment datascience-experiment
